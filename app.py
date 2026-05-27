e556fcf47c2dbb461496cf0c566967bb420489df
nickole1geo <46948613+nickole1geo@users.noreply.github.com>
2026-05-26 22:14:52 -0700

    Update app.py

--- app.py
+++ app.py
@@ -282,12 +282,12 @@ with tab6:
     use the ED as an entry point into care.
     """)
 
-    mental_health_terms = [
-    "Mental Diseases and Disorders",
-    "Mental Diseases & Disorders"
-]
-
-    df["mental_health_dx"] = df["apr_mdc_description"].isin(mental_health_terms)
+   df["mental_health_dx"] = (
+    df["apr_mdc_description"]
+    .astype(str)
+    .str.lower()
+    .str.contains("mental", na=False)
+)
 
     mh_summary = (
     df.groupby("mental_health_dx")["avoidable_ed"]
@@ -311,6 +311,9 @@ with tab6:
 
     mh_df = df[df["mental_health_dx"] == True]
 
+if mh_df.empty:
+    st.warning("No mental health diagnosis records were found in this sample.")
+else:
     mh_payer_summary = (
         mh_df.groupby("payment_typology_1")["avoidable_ed"]
         .mean()
